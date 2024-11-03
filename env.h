dd10a028a4b0ed4691c3ec18bd8d00886d4d5e6f
zapato <zapato@gmail.com>
2024-11-03 20:19:35 +0100

    Enhance: Update declarations for environment variable functions in env.h

--- env.h
+++ env.h
@@ -12,6 +12,8 @@ typedef struct s_env
 t_env *init_env(char **envp);
 void add_env_var(t_env **env_list, char *key, char *value);
 char *get_env_var(t_env *env_list, char *key);
+void update_env_var(t_env **env_list, char *key, char *value);
+void remove_env_var(t_env **env_list, char *key);
 void free_env(t_env *env_list);
 
 #endif // ENV_H
