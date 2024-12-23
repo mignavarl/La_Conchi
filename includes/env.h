9741db5959755003dc271ea3992d8dc130efcaf6
hitchhikergalactic <hitchhikergalactic@gmail.com>
2024-12-23 21:10:37 +0100

    headers actualizados~

--- includes/env.h
+++ includes/env.h
@@ -4,6 +4,7 @@
 # include <stdlib.h>
 # include <string.h>
 # include <stdio.h>
+# include <minishell.h>
 # include "../libft/libft.h"
 
 typedef struct s_env
@@ -15,16 +16,33 @@ typedef struct s_env
 
 // Declaraciones de funciones
 //ENV.C
-t_env	*init_env(char **envp, t_env *env);
-void	add_env_var(t_env **env_list, char *key, char *value);
-char	*get_env_var(t_env *env_list, char *key);
-void	update_env_var(t_env **env_list, char *key, char *value);
-void	remove_env_var(t_env **env_list, char *key);
+t_env	    *init_env(char **envp, t_env *env);
+void	    add_env_var(t_env **env_list, char *key, char *value);
+char	    *get_env_var(t_env *env_list, char *key);
+void	    update_env_var(t_env **env_list, char *key, char *value);
+void	    remove_env_var(t_env **env_list, char *key);
 
 //ENV_UTILS.C
-int		env_lstsize(t_env *lst);
-t_env	*env_last(t_env *lst);
-void	env_add_back(t_env **lst, t_env *new);
-void	free_env(t_env *env_list);
+int		    env_lstsize(t_env *lst);
+t_env	    *env_last(t_env *lst);
+void	    env_add_back(t_env **lst, t_env *new);
+void	    free_env(t_env *env_list);
+
+//BUIILTINS
+void            ft_myprintecho(char *echo, int flag);
+static int      ft_myecho(char *echo);
+t_list	        *builtins(void);
+static int	    echo_aux(char *prompt);
+int	            terminator(t_tools *tools, char *prompt, int blt);
+void	        get_builts(t_string *built, unsigned int i);
+int	            ft_myunset(t_tools *tools, char *name);
+int	            ft_myexport(t_tools *tools, char *prompt);
+char	        *prsstraux(char *aux, char *str, unsigned int *i, t_tools *tools);
+int	            iter_str(char c);
+static void	    ft_printenvp_ii(char *str, int *flag);
+void	        ft_printenvp(t_tools *tools);
+int	            ft_strcmp(char *str1, char *str2);
+int	            ft_customenvp(t_tools *tools);
+char	        *ft_searchqu(char *str, char c, unsigned int *i, t_tools *tools);
 
 #endif // ENV_H
--- includes/minishell.h
+++ includes/minishell.h
@@ -148,6 +148,47 @@ void	free_execve(char **command, t_env *env, char **envp, char *route);
 char	*ft_joinchar(char *str, char c);
 
 //-----------------------ENVIROMENT-----------------------//
+typedef struct s_env
+{
+    char *key;
+    char *value;
+    struct s_env *next;
+} t_env;
+
+// Declaraciones de funciones
+//ENV.C
+t_env	    *init_env(char **envp, t_env *env);
+void	    add_env_var(t_env **env_list, char *key, char *value);
+char	    *get_env_var(t_env *env_list, char *key);
+void	    update_env_var(t_env **env_list, char *key, char *value);
+void	    remove_env_var(t_env **env_list, char *key);
+
+//ENV_UTILS.C
+int		    env_lstsize(t_env *lst);
+t_env	    *env_last(t_env *lst);
+void	    env_add_back(t_env **lst, t_env *new);
+void	    free_env(t_env *env_list);
+
+//BUIILTINS
+//builtins1.c
+void            ft_myprintecho(char *echo, int flag);
+static int      ft_myecho(char *echo);
+t_list	        *builtins(void);
+static int	    echo_aux(char *prompt);
+int	            terminator(t_tools *tools, char *prompt, int blt);
+//builtins2.c
+void	        get_builts(t_string *built, unsigned int i);
+int	            ft_myunset(t_tools *tools, char *name);
+int	            ft_myexport(t_tools *tools, char *prompt);
+char	        *prsstraux(char *aux, char *str, unsigned int *i, t_tools *tools);
+//builtins3.c
+int	            iter_str(char c);
+static void	    ft_printenvp_ii(char *str, int *flag);
+void	        ft_printenvp(t_tools *tools);
+int	            ft_strcmp(char *str1, char *str2);
+int	            ft_customenvp(t_tools *tools);
+char	        *ft_searchqu(char *str, char c, unsigned int *i, t_tools *tools);
+
 
 //-----------------------LEXER FUNCTIONS-----------------------//
 int		lexer(char **words); 
