77e16a5caae1daa343e4665bea17cf2ad5e93b8a
Miguel Navarro Lopez <mignavar@c2r10s5.42madrid.com>
2025-01-09 19:26:23 +0100

    ahora tiene leaks

--- includes/minishell.h
+++ includes/minishell.h
@@ -161,28 +161,6 @@ void	save_fd(t_data *data);
 //FT_JOINCHAR.C
 char	*ft_joinchar(char *str, char c);
 
-//-----------------------ENVIROMENT-----------------------//
-/* typedef struct s_env
-{
-    char *key;
-    char *value;
-    struct s_env *next;
-} t_env;
-
-// Declaraciones de funciones
-//ENV.C
-t_env	    *init_env(char **envp, t_env *env);
-void	    add_env_var(t_env **env_list, char *key, char *value);
-char	    *get_env_var(t_env *env_list, char *key);
-void	    update_env_var(t_env **env_list, char *key, char *value);
-void	    remove_env_var(t_env **env_list, char *key);
-
-//ENV_UTILS.C
-int		    env_lstsize(t_env *lst);
-t_env	    *env_last(t_env *lst);
-void	    env_add_back(t_env **lst, t_env *new);
-void	    free_env(t_env *env_list);
- */
 //-----------------------LEXER FUNCTIONS-----------------------//
 int		lexer(char **words); 
 
